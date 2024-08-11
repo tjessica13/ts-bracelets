11626aea9bf9d57323595270811e49b1d671546f
tjessica13 <jtanottawau@gmail.com>
2024-08-11 16:24:20 -0400

    letter buttons add and remove function

--- components/SongsComponent.tsx
+++ components/SongsComponent.tsx
@@ -3,47 +3,56 @@ import { albums } from '@/data/albums';
 import { useState } from 'react';
 
 const SongsComponent = () => {
-    
-  const [excludeLetters, setExcludeLetters] = useState<string[]>(["e", "p"]);
+    //https://stackoverflow.com/questions/68459964/how-to-create-a-string-array-in-the-usestate-hook-and-update-that-same-array-in
+    const [excludeLetters, setExcludeLetters] = useState<string[]>(["e", "p"]);
 
-  function filterSongs(song: string) {
-    let lowerSong = song.toLowerCase()
-    
-    // https://stackoverflow.com/questions/5582574/how-to-check-if-a-string-contains-text-from-an-array-of-substrings-in-javascript
-    if (!excludeLetters.some(letter => lowerSong.includes(letter))){
-        return song
-    }
-  }
-
-  function handleClick(letter: string) {
-    setExcludeLetters(l => l.filter(item => item !== letter));
-  }
-
-  return (
-    <main>
-      <h1>TS Bracelets</h1>
-        <h1>{excludeLetters}</h1>
-        <button value={"e"} onClick={() => handleClick("e")}>E</button>
-
-      <div>
-        {
-          albums.map((item, i) => (
-            <div>
-              <h2>{i}</h2>
-              <h2>{item.album}</h2>
-              {
-                item.songs.map((song, j) => (
-                  <div>
-                    <h2>{filterSongs(song)}</h2>
-                  </div>
-                ))
-              }
-            </div>
-          ))
+    function filterSongs(song: string) {
+        let lowerSong = song.toLowerCase()
+        
+        // https://stackoverflow.com/questions/5582574/how-to-check-if-a-string-contains-text-from-an-array-of-substrings-in-javascript
+        if (!excludeLetters.some(letter => lowerSong.includes(letter))){
+            return song
         }
-      </div>
-    </main>
-  );
+    }
+
+    function handleClick(letter: string) {
+        if (excludeLetters.includes(letter)){
+            // https://stackoverflow.com/questions/57341541/removing-object-from-array-using-hooks-usestate
+            setExcludeLetters(l => l.filter(item => item !== letter));
+        } else {
+            //https://react.dev/learn/updating-arrays-in-state
+            setExcludeLetters([
+                ...excludeLetters,
+                letter
+            ]);
+        }
+    }
+
+    return (
+        <main>
+        <h1>TS Bracelets</h1>
+            <h1>{excludeLetters}</h1>
+            <button value={"e"} onClick={() => handleClick("e")}>E</button>
+            <button value={"a"} onClick={() => handleClick("a")}>A</button>
+        <div>
+            {
+            albums.map((item, i) => (
+                <div>
+                <h2>{i}</h2>
+                <h2>{item.album}</h2>
+                {
+                    item.songs.map((song, j) => (
+                    <div>
+                        <h2>{filterSongs(song)}</h2>
+                    </div>
+                    ))
+                }
+                </div>
+            ))
+            }
+        </div>
+        </main>
+    );
 }
 
 export default SongsComponent
